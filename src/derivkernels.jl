e32c4b55a22959589a64dbada54c8206d57d1b1a
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2022-11-07 01:17:42 +0000

    Refactor

--- src/derivkernels.jl
+++ src/derivkernels.jl
@@ -2,40 +2,18 @@
 
 abstract type AbstractDerivativeKernel <: Kernel end
 
-struct FirstComponentDerivativeKernel{Tk<:Kernel} <: AbstractDerivativeKernel
-    grad::Function
+struct DerivativeKernel{Tf,Tk<:Kernel} <: AbstractDerivativeKernel
+    d01::Tf
+    d10::Tf
+    d11::Tf
 
     function FirstCompDerivKernel(kernel)
-        dker(t1, t2) = first(first(Zygote.gradient(t1 -> kernel(t1, t2), t1)))
-        return new{typeof(kernel)}(dker)
+        d01(t1, t2) = first(first(Zygote.gradient(t1 -> kernel(t1, t2), t1)))
+        d10(t1, t2) = first(first(Zygote.gradient(t2 -> kernel(t1, t2), t2)))
+        d11(t1, t2) = first(Zygote.hessian(t -> ker(t[1], t[2]), [t1, t2])[2])
+        return new{typeof(d01),typeof(kernel)}(d01, d10, d11)
     end
 end
-
-(dk::FirstCompDerivKernel)(x, y) = dk.grad(x, y)
-
-struct SecondComponentDerivativeKernel{Tk<:Kernel} <: AbstractDerivativeKernel
-    grad::Function
-
-    function SecondCompDerivativeKernel(kernel)
-        kerd(t1, t2) = first(first(Zygote.gradient(t2 -> kernel(t1, t2), t2)))
-        return new{typeof(kernel)}(kerd)
-    end
-end
-
-(dk::SecondCompDerivativeKernel)(x, y) = dk.grad(x, y)
-
-struct BothComponentDerivativeKernel{Tk<:Kernel} <: AbstractDerivativeKernel
-    grad::Function
-
-    function BothCompDerivativeKernel(kernel)
-        function dkerd(t1, t2)
-            return first(Zygote.hessian(t -> ker(t[1], t[2]), [t1, t2])[2])
-        end
-        return new{typeof(kernel)}(dkerd)
-    end
-end
-
-(dk::BothCompDerivativeKernel)(x, y) = dk.grad(x, y)
 
 # need tests, for various kernels and maybe a finitediff method to compare?
 
